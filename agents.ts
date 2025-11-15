083519ddd91cf9337a9b49835be2c9619d2f7b4c
OS-bartmatejczyk <bart.matejczyk@outsystems.com>
2025-11-15 10:55:25 +0100

    added memory function

---
+++ agents.ts
@@ -0,0 +1,18 @@
+import `dotenv/config`;
+import {Langbase} from `langbase`;
+
+const langbase = new Langbase({
+    apiKey :ProcessingInstruction.env.LANGBASE_API_KEY,
+});
+
+export async function runMEmoryAgent(query:string) {
+    const chunks = await langbase.memories.retrieve ({
+        query, 
+        topK: 3, // top 3 only as we dont need more chunks 
+        moemory: [{
+            name:`knowledge-base`, 
+
+        }]
+    })
+    return chunks ; 
+}
